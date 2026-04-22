43ae8c7b96872648acef8abccbc181f20aa02d02
ten <pala@tilde.institute>
2026-04-22 00:16:24 -0400

    checkpoint. riddled with errors. keep going

--- server/src/fs.rs
+++ server/src/fs.rs
@@ -1,9 +1,3 @@
-use std::{
-    path::PathBuf,
-    sync::atomic::AtomicU64,
-    time::{Duration, UNIX_EPOCH},
-};
-
 /// hsyncfs - expose block database as a file system
 ///
 /// why? enabling a new type of topology where the server is actually
@@ -11,11 +5,17 @@ use std::{
 ///
 /// inode format:
 /// [upper 16 bits folder id][lower 48 bits filenames rowid]
-/// structure:
-/// root
-/// |_ <folder id> (inodes will correspond)
-///     |_ <files> ()
+/// when lower 48 bits are zero this means that the ino belongs to a folder
+/// but this does not matter because readdir callback ignores lower bits anyway
 ///
+/// current issue: how to represent subfolders knowing only that
+///                         file names in subfolders contain slash characters
+use std::{
+    path::PathBuf,
+    sync::atomic::AtomicU64,
+    time::{Duration, UNIX_EPOCH},
+};
+
 use r2d2::{Pool, PooledConnection};
 use r2d2_sqlite::SqliteConnectionManager;
 
@@ -33,6 +33,11 @@ struct FileInfo {
     pub namehash: i64,
     pub size: i64,
     pub num_blocks: i64,
+}
+
+struct FolderInfo {
+    pub name: String,
+    pub ino: u64,
 }
 
 impl Fs {
@@ -80,10 +85,56 @@ impl Fs {
         })
     }
 
-    fn file_lookup(&mut self, name: &str) -> anyhow::Result<()> {
+    fn file_lookup(&self, name: &str) -> anyhow::Result<()> {
         let db = self.db_pool.get()?;
 
         Ok(())
+    }
+
+    fn list_root_folders(&self) -> anyhow::Result<Vec<FolderInfo>> {
+        let db = self.db_pool.get()?;
+        let mut folders: Vec<FolderInfo> = vec![];
+
+        let mut stmt = db.prepare("SELECT DISTINCT folder FROM filenames ORDER BY folder ASC")?;
+        let mut folder_entries = stmt.query([])?;
+
+        while let Ok(Some(entry)) = folder_entries.next() {
+            let id = entry.get::<_, i64>(0)? as u64;
+            folders.push(FolderInfo {
+                name: String::from(format!("{}", id)),
+                ino: (id & 0xffff) << 32,
+            });
+        }
+
+        Ok(folders)
+    }
+
+    fn mk_fileattr(
+        req: &fuser::Request,
+        ino: fuser::INodeNo,
+        size: u64,
+        blocks: u64,
+        kind: fuser::FileType,
+        nlink: u32,
+        blksize: u32,
+    ) -> fuser::FileAttr {
+        fuser::FileAttr {
+            ino,
+            size,
+            blocks,
+            atime: UNIX_EPOCH,
+            mtime: UNIX_EPOCH,
+            ctime: UNIX_EPOCH,
+            crtime: UNIX_EPOCH,
+            kind,
+            perm: 0o755,
+            nlink,
+            uid: req.uid(),
+            gid: req.gid(),
+            rdev: 0,
+            blksize,
+            flags: 0,
+        }
     }
 }
 
@@ -92,10 +143,9 @@ impl fuser::Filesystem for Fs {
         &self,
         req: &fuser::Request,
         ino: fuser::INodeNo,
-        fh: Option<fuser::FileHandle>,
+        _fh: Option<fuser::FileHandle>,
         reply: fuser::ReplyAttr,
     ) {
-        // is this fs root
         let folder_id = u64::from(ino) >> 48;
         let file_id = (u64::from(ino) << 16) >> 16;
 
@@ -107,52 +157,29 @@ impl fuser::Filesystem for Fs {
             }
         };
 
+        // if directory/root
         if ino == fuser::INodeNo::ROOT || folder_exists {
-            let attr = fuser::FileAttr {
-                ino,
-                size: 0,
-                blocks: 0,
-                atime: UNIX_EPOCH,
-                mtime: UNIX_EPOCH,
-                ctime: UNIX_EPOCH,
-                crtime: UNIX_EPOCH,
-                kind: fuser::FileType::Directory,
-                perm: 0o755,
-                nlink: 2,
-                uid: 501,
-                gid: 20,
-                rdev: 0,
-                flags: 0,
-                blksize: 512,
-            };
+            let attr = Self::mk_fileattr(req, ino, 0, 0, fuser::FileType::Directory, 2, 512);
 
             reply.attr(&Duration::from_secs(1), &attr);
         } else {
             let file_info = match self.file_info(file_id as i64) {
-                Ok(e) => e,
+                Ok(fi) => fi,
                 Err(_) => {
                     reply.error(fuser::Errno::ENOENT);
                     return;
                 }
             };
 
-            let attr = fuser::FileAttr {
+            let attr = Self::mk_fileattr(
+                req,
                 ino,
-                size: file_info.size as u64,
-                blocks: file_info.num_blocks as u64,
-                atime: UNIX_EPOCH,
-                mtime: UNIX_EPOCH,
-                ctime: UNIX_EPOCH,
-                crtime: UNIX_EPOCH,
-                kind: fuser::FileType::RegularFile,
-                perm: 0o755,
-                nlink: 2,
-                uid: req.uid(),
-                gid: req.gid(),
-                rdev: 0,
-                flags: 0,
-                blksize: 512,
-            };
+                file_info.size as u64,
+                file_info.num_blocks as u64,
+                fuser::FileType::RegularFile,
+                2,
+                512,
+            );
 
             reply.attr(&Duration::from_secs(1), &attr);
         }
@@ -165,40 +192,132 @@ impl fuser::Filesystem for Fs {
         _flags: fuser::OpenFlags,
         reply: fuser::ReplyOpen,
     ) {
-        if ino != fuser::INodeNo::ROOT {
-            reply.error(fuser::Errno::ENOTDIR);
-            return;
-        }
+        let folder_id = u64::from(ino) >> 48;
+
+        match self.dir_exists(folder_id as i64) {
+            Ok(e) if !e && ino != fuser::INodeNo::ROOT => {
+                reply.error(fuser::Errno::ENOTDIR);
+                return;
+            }
+            Err(_) => {
+                reply.error(fuser::Errno::ENOTDIR);
+                return;
+            }
+
+            _ => {}
+        };
 
         reply.opened(
             fuser::FileHandle(self.get_handle()),
-            fuser::FopenFlags::all(),
+            fuser::FopenFlags::all() & !fuser::FopenFlags::FOPEN_PASSTHROUGH,
         );
     }
 
-    // fn lookup(
-    //     &mut self,
-    //     _req: &fuser::Request,
-    //     _parent: u64,
-    //     name: &std::ffi::OsStr,
-    //     reply: fuser::ReplyEntry,
-    // ) {
-    //     tracing::debug!("fs lookup: {}", name.to_str().unwrap());
+    fn lookup(
+        &self,
+        _req: &fuser::Request,
+        parent: fuser::INodeNo,
+        name: &std::ffi::OsStr,
+        reply: fuser::ReplyEntry,
+    ) {
+        tracing::debug!("lookup {} (parent: {})", name.to_string_lossy(), parent);
 
-    //     reply.entry(
-    //         &std::time::Duration::from_secs(1),
-    //         attr,
-    //         fuser::Generation(0),
-    //     );
+        if let Err(e) = (|| {
+            let name = name
+                .to_str()
+                .ok_or(anyhow::anyhow!("encode error"))?
+                .to_string();
 
-    //     // match self.file_lookup(name.to_str().unwrap()) {
-    //     //     Ok(_) => {}
-    //     //     Err(e) => {
-    //     //         tracing::error!("fs lookup: {}", e.to_string());
-    //     //         reply.error(libc::ENOENT);
-    //     //     }
-    //     // };
-    // }
+            if parent == fuser::INodeNo::ROOT {
+                // lookup hsycnfs (parent: 1)
+                if name == FS_NAME {
+                    let attr = Self::mk_fileattr(
+                        req,
+                        fuser::INodeNo::ROOT,
+                        0,
+                        0,
+                        fuser::FileType::Directory,
+                        2,
+                        512,
+                    );
+
+                    // wtf is generation
+                    reply.entry(&Duration::from_secs(1), &attr, fuser::Generation(0));
+                } else {
+                    // otherwise it should be a folder id which is just a number
+                    // because we are still in root folder
+                    let folder_id = u64::try_from(name)? as i64;
+
+                    match self.dir_exists(folder_id) {
+                        Ok(e) if e => {}
+
+                        _ => {
+                            reply.error(fuser::Errno::ENOENT);
+                            anyhow::bail!("directory does not exist");
+                        }
+                    }
+                }
+            }
+
+            anyhow::Ok(())
+        })() {
+            tracing::error!("lookup error: {}", e.to_string());
+        }
+    }
+
+    fn readdir(
+        &self,
+        _req: &fuser::Request,
+        ino: fuser::INodeNo,
+        fh: fuser::FileHandle,
+        offset: u64,
+        mut reply: fuser::ReplyDirectory,
+    ) {
+        // if the root directory, list all the folders
+        if ino == fuser::INodeNo::ROOT {
+            match self.list_root_folders() {
+                Ok(folders) => {
+                    for (index, folder) in folders.iter().skip(offset as usize).enumerate() {
+                        let res = reply.add(
+                            fuser::INodeNo(folder.ino),
+                            offset + index as u64 + 1,
+                            fuser::FileType::Directory,
+                            &folder.name,
+                        );
+
+                        tracing::debug!(
+                            "\tfolder ino {} name {}: {}",
+                            folder.ino,
+                            folder.name,
+                            res
+                        );
+
+                        if res {
+                            break;
+                        }
+                    }
+                }
+
+                Err(_) => {
+                    reply.error(fuser::Errno::ENOENT);
+                    return;
+                }
+            }
+        } else {
+            // not a root dir, let's read folder id
+            let folder_id = u64::from(ino) >> 48;
+
+            match self.dir_exists(folder_id as i64) {
+                Ok(e) if e => {}
+
+                _ => {
+                    reply.error(fuser::Errno::ENOENT);
+                }
+            }
+        }
+
+        reply.ok();
+    }
 }
 
 pub async fn hsyncfs_create(rootpath: PathBuf, db_pool: Pool<SqliteConnectionManager>) {
